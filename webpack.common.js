b6b6ba73bdee4424f8b8a43cc1120e498cdc2d01
hltree <kiona.n31@icloud.com>
2019-12-22 11:11:15 +0900

    update: add&edit modules

---
+++ webpack.common.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: {
+        'main.js': './src/js/main.js',
+        'style.css': './src/scss/style.scss',
+        'index.pug': './src/pug/index.pug'
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        filename: '[name]'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            'presets': [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        'useBuiltIns': 'entry'
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'css-hot-loader', MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            plugins: () => [require('autoprefixer')]
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.pug$/,
+                use: 'pug-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/pug/index.pug'
+        }),
+        new MiniCssExtractPlugin()
+    ]
+}
